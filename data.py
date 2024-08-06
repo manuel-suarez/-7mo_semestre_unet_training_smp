b20dde2696964e23cc2a9d3782087151fe8a16e4
Arturo Suárez <manuel.suarez@cimat.mx>
2024-08-06 11:12:28 -0600

    Data model definition

---
+++ data.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+from skimage.io import imread
+from torch.utils.data import Dataset
+
+
+class CimatDataset(Dataset):
+    def __init__(
+        self,
+        keys,
+        features_path,
+        features_ext,
+        features_channels,
+        labels_path,
+        labels_ext,
+        dims,
+    ):
+        super().__init__()
+        self.keys = keys
+        self.fetures_path = features_path
+        self.labels_path = labels_path
+        self.features_channels = features_channels
+        self.features_ext = features_ext
+        self.labels_ext = labels_ext
+        self.dims = dims
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, idx):
+        x = np.zeros(self.dims, dtype=np.float32)
+        # Load features
+        for j, feature in enumerate(self.features_channels):
+            filename = os.path.join(self.features_path, feature, key + self.feature_ext)
+            z = imread(filename, as_gray=True).astype(np.float32)
+
+            if z.shape[0] == self.dims[0] and z.shape[1] == self.dims[1]:
+                x[..., j] = z
+        # Load label
+        filename = os.path.join(self.labels_path, key + self.label_ext)
+        y = np.zeros((x.shape[0], x.shape[1], 1))
+        z = imread(filename, as_gray=True).astype(np.float32) / 255.0
+
+        if z.shape[0] == self.dims[0] and z.shape[1] == self.dims[1]:
+            y[..., 0] = z
+
+        return x, y
