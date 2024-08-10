c9e12c51b11001754898a4ec99b1344980814dbc
Arturo Suárez <manuel.suarez@cimat.mx>
2024-08-10 13:14:59 -0600

    Added metrics

--- module.py
+++ module.py
@@ -1,4 +1,5 @@
 import lightning as L
+import torchmetrics
 from torch import optim, nn, utils, Tensor
 
 
@@ -8,13 +9,47 @@ class CimatModule(L.LightningModule):
         self.model = model
         self.optimizer = optimizer
         self.loss_fn = loss_fn
+        self.train_accuracy = torchmetrics.Accuracy(task="binary")
+        self.valid_accuracy = torchmetrics.Accuracy(task="binary")
+        self.train_meaniou = torchmetrics.segmentation.MeanIoU(
+            num_classes=2, per_class=True
+        )
+        self.valid_meaniou = torchmetrics.segmentation.MeanIoU(
+            num_classes=2, per_class=True
+        )
 
     def training_step(self, batch, batch_idx):
         inputs, labels = batch
         preds = self.model(inputs)
-        loss = self.loss_fn(preds, labels)
-        self.log("train_loss", loss)
-        return loss
+        train_loss = self.loss_fn(preds, labels)
+        self.train_accuracy(preds, labels)
+        self.train_meaniou(preds, labels)
+        self.log_dict(
+            {
+                "train_loss": train_loss,
+                "train_acc": self.train_accuracy,
+                "train_meaniou": self.train_meaniou,
+            },
+            prog_bar=True,
+        )
+        return train_loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = batch
+        preds = self.model(inputs)
+        valid_loss = self.loss_fn(preds, labels)
+        self.valid_accuracy(preds, labels)
+        self.valid_meaniou(preds, labels)
+        self.log_dict(
+            {
+                "valid_loss": valid_loss,
+                "valid_acc": self.valid_accuracy,
+                "valid_meaniou": self.valid_meaniou,
+            },
+            prog_bar=True,
+            sync_dist=True,
+        )
+        return valid_loss
 
     def configure_optimizers(self):
         return self.optimizer
